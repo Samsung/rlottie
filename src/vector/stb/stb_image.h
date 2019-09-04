9afdb8dc46f0cf5fc0bac9979ffff9edf2b6683a
Hermet Park <hermetpark@gmail.com>
2019-09-04 17:26:26 +0900

    stb: added exceptional handling.
    ftell() could return -1  but fseek() doesn't allow minus values.
    
    Modified to skip irritating coverity report.

--- src/vector/stb/stb_image.h
+++ src/vector/stb/stb_image.h
@@ -7187,7 +7187,7 @@ STBIDEF int stbi_info_from_file(FILE *f, int *x, int *y, int *comp)
    long pos = ftell(f);
    stbi__start_file(&s, f);
    r = stbi__info_main(&s,x,y,comp);
-   fseek(f,pos,SEEK_SET);
+   if (pos >= 0) fseek(f,pos,SEEK_SET);
    return r;
 }
 
