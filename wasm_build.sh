ea0a3aa6d67da1625247dadce55567b684f743f2
Subhransu Mohanty <sub.mohanty@samsung.com>
2020-07-21 10:43:52 +0900

    wasm: update the build flag to reduce generated wasm file size

--- wasm_build.sh
+++ wasm_build.sh
@@ -8,11 +8,11 @@ fi
 
 if [ ! -d "./builddir_wasm" ]; then
     sed "s|EMSDK:|$1|g" wasm_cross.txt > /tmp/.wasm_cross.txt
-    meson -Dthread=false -Dmodule=false -Dcache=false -Dexample=false -Ddefault_library=static builddir_wasm --cross-file /tmp/.wasm_cross.txt
+    meson -Dthread=false -Dmodule=false -Dcache=false -Dexample=false -Db_lto=true -Ddefault_library=static builddir_wasm --cross-file /tmp/.wasm_cross.txt
     cp ./test/wasm_test.html builddir_wasm/src/index.html
 fi
 
 sudo ninja -C builddir_wasm/
 echo "RESULT:"
 echo " rlottie-wasm.wasm and rlottie-wasm.js can be found in builddir_wasm/src folder"
-
+ls -lrt builddir_wasm/src/rlottie-wasm.*
