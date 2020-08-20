1e1f5c2274e5b58fd4f3be43f5d1065a545a5d48
Subhransu Mohanty <sub.mohanty@samsung.com>
2020-08-20 10:28:56 +0900

    fixed sign extension warning

--- src/vector/vdrawhelper.cpp
+++ src/vector/vdrawhelper.cpp
@@ -537,7 +537,7 @@ static inline void process_in_chunk(const VRle::Span *array, size_t size,
     for (size_t i = 0; i < size; i++) {
         const auto &span = array[i];
         size_t      len = span.len;
-        size_t      x = span.x;
+        auto        x = span.x;
         while (len) {
             auto l = std::min(len, buf.size());
             process(buf.data(), x, span.y, l, span.coverage);
