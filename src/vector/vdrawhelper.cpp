875626965959d8e269ca22175c8e1ad190696c43
Nicholas Guriev <nicholas@guriev.su>
2022-01-04 20:15:50 +0300

    Check border of color table while generating gradient

--- src/vector/vdrawhelper.cpp
+++ src/vector/vdrawhelper.cpp
@@ -147,7 +147,7 @@ bool VGradientCache::generateGradientColorTable(const VGradientStops &stops,
 
     colorTable[pos++] = curColor;
 
-    while (fpos <= curr->first) {
+    while (fpos <= curr->first && pos < size) {
         colorTable[pos] = colorTable[pos - 1];
         pos++;
         fpos += incr;
